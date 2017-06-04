ff877975319a9f305d4e89f96c8a997b0992718a
HKeating <horacekeating@hotmail.co.uk>
2017-06-04 18:59:54 +0100

    dashboard functionality getting there

--- src/js/controllers/dashboard.controller.js
+++ src/js/controllers/dashboard.controller.js
@@ -2,8 +2,8 @@ angular
   .module('ProjectFour')
   .controller('DashboardCtrl', DashboardCtrl);
 
-DashboardCtrl.$inject = ['CurrentUserService', '$rootScope', 'Project', '$scope'];
-function DashboardCtrl(CurrentUserService, $rootScope, Project, $scope) {
+DashboardCtrl.$inject = ['CurrentUserService', '$rootScope', 'Project'];
+function DashboardCtrl(CurrentUserService, $rootScope, Project) {
   const vm = this;
   vm.title = 'Dashboard page';
   vm.user = CurrentUserService.currentUser;
@@ -11,13 +11,17 @@ function DashboardCtrl(CurrentUserService, $rootScope, Project, $scope) {
     $rootScope.$on('loggedIn', () => {
       vm.user = CurrentUserService.currentUser;
       vm.projects = vm.user.projects;
-      console.log(vm.projects);
     });
   }
+  // $rootScope is like a global event listener/trigger across the whole app.
+  // You use $rootScope.$broadcast('Your Message'); as the trigger
+  // and $rootScope.$on('Your Message', () => { Triggered function }); as the listener.
   $rootScope.$on('Project Change', () => {
     CurrentUserService.getUser();
+    vm.newProject = {};
+    vm.projectToUpdate = {};
   });
-  $scope.$projectForm = $('#projectForm');
+
   vm.createProject = createProject;
   function createProject() {
     vm.newProject.user_id = vm.user.id;
@@ -27,8 +31,6 @@ function DashboardCtrl(CurrentUserService, $rootScope, Project, $scope) {
       .then((data) => {
         console.log('New project created: ', data);
         $rootScope.$broadcast('Project Change');
-        $scope.$projectForm.setPristine();
-        $scope.$projectForm.setUntouched();
       });
   }
 
@@ -38,9 +40,35 @@ function DashboardCtrl(CurrentUserService, $rootScope, Project, $scope) {
     .delete({ id: project.id })
     .$promise
     .then(() => {
-      console.log('Project deleted');
       $rootScope.$broadcast('Project Change');
     });
+  }
+
+  vm.showEditForm = false;
+  vm.selectToEdit = selectToEdit;
+  function selectToEdit(projectId) {
+    vm.showEditForm = true;
+    vm.projectToUpdate = Project.get({id: projectId});
+  }
+
+  vm.hideEditForm = hideEditForm;
+  function hideEditForm() {
+    vm.showEditForm = false;
+    vm.projectToUpdate = {};
+  }
+
+
+  vm.updateProject = updateProject;
+  function updateProject(project) {
+    // if (vm.editForm.$valid) {
+    Project
+      .update({id: project.id }, project)
+      .$promise
+      .then(() => {
+        $rootScope.$broadcast('Project Change');
+        vm.showEditForm = false;
+      });
+    // }
   }
 
 }
