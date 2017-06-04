55102e6d8ccda7ca78e5b0329391a82d98a25064
kartuzovmax <kartuzovmax@hotmail.com>
2017-06-04 17:03:45 +0500

    Adding a start point of the line

--- src/js/controllers/project.controller.js
+++ src/js/controllers/project.controller.js
@@ -6,4 +6,19 @@ ProjectCtrl.$inject = [];
 function ProjectCtrl() {
   const vm = this;
   vm.title = 'Project page';
+  vm.days = 7;
+
+  vm.milestones = [
+    {
+      deadline: 3,
+      title: 'MVP'
+    },
+    {
+      deadline: 5,
+      title: 'Beta'
+    }
+  ];
+
+  
+
 }
