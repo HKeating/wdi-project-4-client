49370eb6c7d932213e14c7714d0651dc56247386
HKeating <horacekeating@hotmail.co.uk>
2017-06-04 17:21:18 +0100

    project creation working

--- src/js/controllers/dashboard.controller.js
+++ src/js/controllers/dashboard.controller.js
@@ -2,17 +2,32 @@ angular
   .module('ProjectFour')
   .controller('DashboardCtrl', DashboardCtrl);
 
-DashboardCtrl.$inject = ['CurrentUserService', '$rootScope'];
-function DashboardCtrl(CurrentUserService, $rootScope) {
+DashboardCtrl.$inject = ['CurrentUserService', '$rootScope', 'Project'];
+function DashboardCtrl(CurrentUserService, $rootScope, Project) {
   const vm = this;
   vm.title = 'Dashboard page';
   vm.user = CurrentUserService.currentUser;
   if(!vm.user) {
     $rootScope.$on('loggedIn', () => {
       vm.user = CurrentUserService.currentUser;
+      vm.projects = vm.user.projects;
     });
   }
-  vm.projects = vm.user.projects;
+  $rootScope.$on('New Project', () => {
+    CurrentUserService.getUser();
+  });
 
+  vm.createProject = createProject;
+
+  function createProject() {
+    vm.newProject.user_id = vm.user.id;
+    Project
+      .save(vm.newProject)
+      .$promise
+      .then((data) => {
+        console.log('New project created: ', data);
+        $rootScope.$broadcast('New Project');
+      });
+  }
 
 }
