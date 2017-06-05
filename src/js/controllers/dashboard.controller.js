dff2c4fff91def463f9e50943d65e7c6643ebd7e
HKeating <horacekeating@hotmail.co.uk>
2017-06-05 10:57:17 +0100

    some more tweaks

--- src/js/controllers/dashboard.controller.js
+++ src/js/controllers/dashboard.controller.js
@@ -1,40 +1,38 @@
 angular
-  .module('ProjectFour')
-  .controller('DashboardCtrl', DashboardCtrl);
+.module('ProjectFour')
+.controller('DashboardCtrl', DashboardCtrl);
 
 DashboardCtrl.$inject = ['CurrentUserService', '$rootScope', 'Project'];
 function DashboardCtrl(CurrentUserService, $rootScope, Project) {
   const vm = this;
   vm.title = 'Dashboard page';
   vm.user = CurrentUserService.currentUser;
-  // if(!vm.user) {
-    $rootScope.$on('loggedIn', () => {
-      vm.user = CurrentUserService.currentUser;
-      vm.projects = vm.user.projects;
-    });
-  // }
+  // When loggedIn fires refresh currentUser - allows live updating of projects
+  $rootScope.$on('loggedIn', () => {
+    vm.user = CurrentUserService.currentUser;
+    vm.projects = vm.user.projects;
+  });
+
   // $rootScope is like a global event listener/trigger across the whole app.
   // You use $rootScope.$broadcast('Your Message'); as the trigger
   // and $rootScope.$on('Your Message', () => { Triggered function }); as the listener.
   $rootScope.$on('Project Change', () => {
+    // On project change (CRUD) refresh current user, clear newProject and projectToUpdate objects
     CurrentUserService.getUser();
-
-    vm.user = CurrentUserService.currentUser;
     vm.newProject = {};
     vm.projectToUpdate = {};
-
   });
 
   vm.createProject = createProject;
   function createProject() {
     vm.newProject.user_id = vm.user.id;
     Project
-      .save(vm.newProject)
-      .$promise
-      .then((data) => {
-        console.log('New project created: ', data);
-        $rootScope.$broadcast('Project Change');
-      });
+    .save(vm.newProject)
+    .$promise
+    .then((data) => {
+      console.log('New project created: ', data);
+      $rootScope.$broadcast('Project Change');
+    });
   }
 
   vm.deleteProject = deleteProject;
@@ -50,6 +48,7 @@ function DashboardCtrl(CurrentUserService, $rootScope, Project) {
   vm.showEditForm = false;
   vm.selectToEdit = selectToEdit;
   function selectToEdit(projectId) {
+    // Use id of project where edit button was clicked to find that project in db
     vm.showEditForm = true;
     vm.projectToUpdate = Project.get({id: projectId});
   }
@@ -60,17 +59,16 @@ function DashboardCtrl(CurrentUserService, $rootScope, Project) {
     vm.projectToUpdate = {};
   }
 
-
   vm.updateProject = updateProject;
   function updateProject(project) {
     // if (vm.editForm.$valid) {
     Project
-      .update({id: project.id }, project)
-      .$promise
-      .then(() => {
-        $rootScope.$broadcast('Project Change');
-        vm.showEditForm = false;
-      });
+    .update({id: project.id }, project)
+    .$promise
+    .then(() => {
+      $rootScope.$broadcast('Project Change');
+      vm.showEditForm = false;
+    });
     // }
   }
 
