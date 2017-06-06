820c6d11b797c8441344282665b21414411a4fac
h6gs <harrystirling94@gmail.com>
2017-06-06 21:33:01 +0100

    ship moving

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -29,4 +29,28 @@ function MainCtrl($rootScope, CurrentUserService, $state, $scope) {
 
   $scope.$state = $state;
 
+  //var container = document.getElementById('container');
+
+  // var square1 = document.getElementById('square1');
+  // var square2 = document.getElementById('square2');
+
+
+  // $scope.$square1 = $('#square1');
+  // $scope.$square2 = $('#square2');
+
+  // $scope.$square1 = $('#square1');
+  // $scope.$square2 = $('#square2');
+  var windowHeight = window.innerHeight;
+  var windowWidth = window.innerWidth;
+  var scrollArea = 0 - windowHeight;
+
+  // update position of square 1 and square 2 when scroll event fires.
+  window.addEventListener('scroll', function() {
+    var scrollTop = window.pageYOffset || window.scrollTop;
+    var scrollPercent = scrollTop/scrollArea || 0;
+
+    square1.style.left = scrollPercent * windowWidth + 'px';
+    square2.style.left = 100 - scrollPercent*windowWidth * 0.5 + 'px';
+  });
+
 }
