e4abb3e22c52ceac4ec0545323031812e76875bd
h6gs <harrystirling94@gmail.com>
2017-06-08 19:11:00 +0100

    dashboard & home tweaked

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -20,7 +20,7 @@ function MainCtrl($rootScope, CurrentUserService, $state, $scope) {
   };
   $rootScope.$on('loggedOut', () => {
     vm.user = null;
-    $state.go('login');
+    $state.go('home');
   });
 
   vm.goToUserProfile = () => {
@@ -61,5 +61,30 @@ function MainCtrl($rootScope, CurrentUserService, $state, $scope) {
     // }
 
   });
+
+  $('input[type="submit"]').mousedown(function(){
+    $(this).css('background', '#2ecc71');
+  });
+  $('input[type="submit"]').mouseup(function(){
+    $(this).css('background', '#1abc9c');
+  });
+
+  $('#loginform').click(function(){
+    $('.login').fadeToggle('slow');
+    $(this).toggleClass('green');
+  });
+
+
+
+  $(document).mouseup(function (e){
+    var container = $('.login');
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+    && container.has(e.target).length === 0){
+      container.hide();
+      $('#loginform').removeClass('green');
+    }
+  });
+
 
 }
